4ac10aed33909c33f9d33b4f92672a0895e038ef
Ernesto Angulo <ernestoangulojimenez@gmail.com>
2020-10-01 23:07:38 -0500

    passport installed and scripts for adding movies, users and API TOKENS added

--- config/index.js
+++ config/index.js
@@ -8,6 +8,11 @@ const config = {
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  authJWTSecret: process.env.AUTH_JWT_SECRET,
+  publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN,
 };
 
 module.exports = {
